538536a379271da082ea5538ac97b499dd5d22ad
kaizen <admin@kaizen.wtf>
2023-09-24 12:19:44 -0400

    Sun Sep 24 12:14:49 PM EDT 2023

--- .config/zsh/prompt.zsh
+++ .config/zsh/prompt.zsh
@@ -7,14 +7,14 @@ path+="$ZDOTDIR"/prompts
 # [[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
 
 ## Pure prompt
-fpath+=($ZDOTDIR/prompts/pure)
-autoload -Uz promptinit; promptinit
-prompt pure
-zstyle :prompt:pure:git:stash show yes
+# fpath+=($ZDOTDIR/prompts/pure)
+# autoload -Uz promptinit; promptinit
+# prompt pure
+# zstyle :prompt:pure:git:stash show yes
 
 ## Starship prompt
-# if [[ $(command -v starship) ]]; then
-#   eval "$(starship init zsh)"
-# else
-#   echo "No prompt not installed"
-# fi
+if [[ $(command -v starship) ]]; then
+  eval "$(starship init zsh)"
+else
+  echo "No prompt not installed"
+fi
